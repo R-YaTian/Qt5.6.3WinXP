242c21c550aa60a949840efd3169d97ff7d90c44
R-YaTian <darkbluesky@outlook.com>
2023-09-27 18:15:37 +0800

    Update some build flags

--- build.bat
+++ build.bat
@@ -1,12 +1,12 @@
 REM this script assumes that the Microsoft.VisualStudio.Component.WinXP
 REM component is available in the local Visual Studio installation.
 
-call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64_x86 10.0.10240.0 -vcvars_ver=14.16
+call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
 
 REM https://devblogs.microsoft.com/cppblog/windows-xp-targeting-with-c-in-visual-studio-2012/
-set INCLUDE=%ProgramFiles(x86)%\Microsoft SDKs\Windows\v7.1A\Include;D:\OpenSSL\include;%INCLUDE%
+set INCLUDE=%ProgramFiles(x86)%\Microsoft SDKs\Windows\v7.1A\Include;C:\OpenSSL\include;%INCLUDE%
 set PATH=%ProgramFiles(x86)%\Microsoft SDKs\Windows\v7.1A\Bin;%PATH%
-set LIB=%ProgramFiles(x86)%\Microsoft SDKs\Windows\v7.1A\Lib;D:\OpenSSL\lib;%LIB%
+set LIB=%ProgramFiles(x86)%\Microsoft SDKs\Windows\v7.1A\Lib;C:\OpenSSL\lib;%LIB%
 
 REM the additional linker parameters are taken care of by qmake, since the
 REM -target xp switch causes the configure script to set the QMAKE_TARGET_OS
@@ -15,21 +15,21 @@ REM ",5.01", which is then passed as the extra suffix to the linker's subsystem
 REM parameter.
 
 REM harfbuzz is disabled because of QTBUG-38913
-call configure.bat -prefix D:\Qt\5.6.3XP\ -opensource -confirm-license^
- -release -nomake examples -nomake tests -target xp^
+call configure.bat -prefix C:\Qt\5.6.3XP\ -opensource -confirm-license^
+ -release -nomake examples -nomake tests -target xp -mp -no-pch^
  -no-harfbuzz -no-sse3 -no-ssse3 -no-sse4.1 -no-sse4.2 -no-avx -no-avx2^
- -platform win32-msvc2017 -no-angle -opengl desktop -openssl-linked
+ -platform win32-msvc2013 -no-angle -opengl desktop -openssl-linked
 
 7z x jom.zip
 
-mkdir D:\Qt
+mkdir C:\Qt
 jom /J 4
 nmake
 nmake install
-copy /Y D:\OpenSSL\bin\libeay32.dll D:\Qt\5.6.3XP\bin\libeay32.dll
-copy /Y D:\OpenSSL\bin\ssleay32.dll D:\Qt\5.6.3XP\bin\ssleay32.dll
-xcopy /Y /S /Q D:\OpenSSL\lib D:\Qt\5.6.3XP\lib
+copy /Y C:\OpenSSL\bin\libeay32.dll C:\Qt\5.6.3XP\bin\libeay32.dll
+copy /Y C:\OpenSSL\bin\ssleay32.dll C:\Qt\5.6.3XP\bin\ssleay32.dll
+xcopy /Y /S /Q C:\OpenSSL\lib C:\Qt\5.6.3XP\lib
 
-cd /d D:\Qt
+cd /d C:\Qt
 7z a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on -sse Qt-5.6.3-XP.7z 5.6.3XP
 pause
--- build_shared.bat
+++ build_shared.bat
@@ -5,12 +5,12 @@ powershell -Command^
  "Invoke-WebRequest https://github.com/R-YaTian/openssl102u-xp/raw/master/OpenSSL-1.0.2u-XP.7z -O openssl.7z"
 7z x -oC:\ openssl.7z
 
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars32.bat"
+call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64_x86 -vcvars_ver=14.16
 
 REM https://devblogs.microsoft.com/cppblog/windows-xp-targeting-with-c-in-visual-studio-2012/
-set INCLUDE=%ProgramFiles(x86)%\Microsoft SDKs\Windows\7.1A\Include;C:\OpenSSL\include;%INCLUDE%
-set PATH=%ProgramFiles(x86)%\Microsoft SDKs\Windows\7.1A\Bin;%PATH%
-set LIB=%ProgramFiles(x86)%\Microsoft SDKs\Windows\7.1A\Lib;C:\OpenSSL\lib;%LIB%
+set INCLUDE=%ProgramFiles(x86)%\Microsoft SDKs\Windows\v7.1A\Include;C:\OpenSSL\include;%INCLUDE%
+set PATH=%ProgramFiles(x86)%\Microsoft SDKs\Windows\v7.1A\Bin;%PATH%
+set LIB=%ProgramFiles(x86)%\Microsoft SDKs\Windows\v7.1A\Lib;C:\OpenSSL\lib;%LIB%
 
 REM the additional linker parameters are taken care of by qmake, since the
 REM -target xp switch causes the configure script to set the QMAKE_TARGET_OS
